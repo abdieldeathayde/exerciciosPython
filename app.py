3208c1ed371bf16ff2ce58f2c4e029d737ae2802
Abdiel Athayde <abdieldeathayde67@gmail.com>
2022-01-13 12:32:46 -0300

    feat: add first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+# print('Olá mundo! ')
+
+# a = 2
+# b = 3
+# soma = a + b
+# print(soma)
+
+# nome = "Abdiel Athayde"
+# idade = 22
+
+# print(f"Olá {nome}, sua idade é {idade} anos")
+#print(type(nome_cliente))
+
+# texto  = 'Abdiel está se formando'
+# btexto = True
+# if (btexto == True):
+#     print('Parabéns ' + texto)
+# else:
+#     print("Quase la")
+
+# if 2 == 3:
+#     print('oi')
+#     print('Tudo bem?')
+# elif 4 == 3:
+#     print('To aqui')
+# elif 5 == 7 :
+#     print('oi chat')
+# else:
+#     print('To no else')
+# else:
+#     print('Como vai você?')
+#print('To fora!')
+
+# nota1 = float(input('Digite uma nota: '))
+# nota2 = float(input('Digite outra nota: '))
+
+# media = (nota1 + nota2) / 2
+
+# if media >=6:
+#     print('APROVADO')
+# elif media >=4:
+#     print('RECUPERAÇÃO')
+# elif media < 4:
+#     print('REPROVADO')
+    
+x = 0
+while x < 10:
+    print('Olá')
+    x = x + 1
